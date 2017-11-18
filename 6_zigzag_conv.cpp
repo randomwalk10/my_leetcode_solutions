2788a62700f0897b597bdf1554cea469001bd891
Xuemeng Zhang <randomwalk10@Xuemengs-MacBook-Pro.local>
2017-11-17 19:56:11 -0700

    6 zigzag ver 1.0

--- 6_zigzag_conv.cpp
+++ 6_zigzag_conv.cpp
@@ -18,33 +18,31 @@ class Solution {
 			const int str_len = r.size();
 			if(numRows<=1) return r;
 			//get the total cycles and length of each cycle
-			int cycle_len = numRows + numRows - 2;
+			int cycle_len = 2 * numRows - 2;
 			int cycle_num = (str_len%cycle_len) ? (str_len/cycle_len+1) : (str_len/cycle_len);
 			//perform conversion
 			for (int i = 0; i < str_len; ++i) {
 				//determine the cycle index and the index within the cycle
 				int cycle_index = i/cycle_len;
 				int index_within = i%cycle_len;
+				int first_idx = (index_within<numRows) ? \
+								index_within : (cycle_len-index_within);
 				//determine the index of new string and replace old char with new char
-				int new_idx = 0;
-				for (int j = 0; j < (index_within%cycle_len); ++j) {
-					new_idx += ((j+numRows)<cycle_len) ? (2*cycle_num-2) : (cycle_num-1);	
-					new_idx += ((cycle_num-1)*cycle_len+j<str_len) ? 1 : 0;
-					new_idx += ((cycle_num-1)*cycle_len+j+numRows<str_len) ? 1 : 0;
+				int cnt = 0;
+				for (int j = 0; j < first_idx; ++j) {
+					cnt += (0!=j) ? (2*cycle_num-2) : (cycle_num-1);	
+					cnt += ((cycle_num-1)*cycle_len+j<str_len) ? 1 : 0;
+					cnt += ((cycle_num-1)*cycle_len+cycle_len-j<str_len) ? 1 : 0;
 				}
-				if(index_within>=numRows){
-					new_idx += 2*(cycle_index+1);
-				}
-				else if( (index_within+numRows)>=cycle_len ){
-					new_idx += cycle_index+1;
-				}
-				else if( cycle_index<=0 ){
-					new_idx += 1;
+				if( (index_within==0) || \
+						(index_within==(numRows-1)) ){
+					cnt += cycle_index+1;
 				}
 				else{
-					new_idx += 2*(cycle_index-1) + 1;
+					cnt += (first_idx==index_within) ? \
+							   (2*cycle_index+1) : (2*cycle_index+2);
 				}
-				r[new_idx] = s[i];
+				r[cnt-1] = s[i];
 			}
 			//return
 			return r;
