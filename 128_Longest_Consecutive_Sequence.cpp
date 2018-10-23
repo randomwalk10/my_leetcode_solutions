8a3821a38df3fbe46ed085937449754c8d6d692a
Xuemeng Zhang <nxzhangxm@gmail.com>
2018-10-23 13:18:29 -0500

    oct 23, 2018: -optimized prob #128; -used unordered_set; -O(n) in time and O(n) in space

--- 128_Longest_Consecutive_Sequence.cpp
+++ 128_Longest_Consecutive_Sequence.cpp
@@ -10,50 +10,30 @@ Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 */
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
-private:
-	void radix_sort(vector<int>& nums, vector<int>& out){
-		out.assign(nums.begin(), nums.end());
-		vector<int> temp;
-
-		//sort
-		for(int bit_offset=0; bit_offset<32; ++bit_offset){
-			int bit_mask = (1<<bit_offset);
-			int one_cnt=0;
-			temp.assign(out.begin(), out.end());
-			for(int i=0; i<(int)nums.size(); ++i){
-				if(bit_mask&temp[i]) one_cnt++;
-			}
-
-			int l = 0;
-			int r = (bit_offset<31) ? (int)nums.size()-one_cnt : one_cnt;
-			for(int i=0; i<(int)nums.size(); ++i){
-				if((bit_offset<31)&&(bit_mask&temp[i]))
-					out[r++] = temp[i];
-				else if(bit_offset<31)
-					out[l++] = temp[i];
-				else if(bit_mask&temp[i])
-					out[l++] = temp[i];
-				else
-					out[r++] = temp[i];
-			}
-		}
-	}
 public:
     int longestConsecutive(vector<int>& nums) {
-		if(nums.size()<2) return nums.size();
-		vector<int> ranked;
-
-		radix_sort(nums, ranked);
-
-		int cnt = 1;
+		if(nums.empty()) return 0;
+		unordered_set<int> _set;
 		int max_cnt = 1;
-		for(int i=0; i<(int)ranked.size()-1; ++i){
-			cnt = (ranked[i+1]==ranked[i]+1)?cnt+1: \
-				  (ranked[i+1]==ranked[i])?cnt:1;
-			max_cnt = (cnt>max_cnt)?cnt:max_cnt;
+
+		for(size_t i=0; i<nums.size(); ++i){
+			_set.insert(nums[i]);
+		}
+
+		for(unordered_set<int>::iterator iter=_set.begin();
+				iter!=_set.end(); ++iter){
+			if(_set.find(*iter-1)==_set.end()){
+				int temp_cnt = 1;
+				int curNum = *iter;
+				while(_set.find(++curNum)!=_set.end()){
+					temp_cnt++;
+				}
+				max_cnt = (temp_cnt>max_cnt) ? temp_cnt : max_cnt;
+			}
 		}
 
 		return max_cnt;
