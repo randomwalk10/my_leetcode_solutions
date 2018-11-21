e02b5ddfa32bc6227b19eb251ae8819a99c1def5
Xuemeng Zhang <nxzhangxm@gmail.com>
2018-11-20 18:40:27 -0600

    nov 20, 2018: -modify prob #85; -O(N^2) in time and O(N) in space; -use stack

--- 85_Maximal_Rectangle.cpp
+++ 85_Maximal_Rectangle.cpp
@@ -13,6 +13,7 @@ Input:
 Output: 6
 */
 #include <vector>
+#include <stack>
 using namespace std;
 
 class Solution {
@@ -20,25 +21,37 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
 		if(matrix.empty()) return 0;
 		int res = 0;
-		vector<int> heights(matrix[0].size(), 0);
+		stack<int> s;
+		vector<int> heights(matrix[0].size()+1, 0);
+		int l;
 
 		for(int r=0; r<(int)matrix.size(); ++r){
-			for(int c=0; c<(int)matrix[0].size(); ++c){
-				if(matrix[r][c]!='0'){
-					//update res
-					int min_height = heights[c]+1;
-					int temp_c = c;
-					while( (temp_c<(int)matrix[0].size()) && \
-							(matrix[r][temp_c]!='0') ){
-						min_height = min(min_height, heights[temp_c]+1);
-						res = max(res, (temp_c-c+1) * min_height);
-						temp_c++;
+			//update heights
+			for(int c=0; c<(int)heights.size(); ++c){
+				if(c!=((int)heights.size()-1)){
+					if(matrix[r][c]!='0'){
+						heights[c] += 1;
 					}
-					//update heights
-					heights[c] += 1;
+					else{
+						heights[c] = 0;
+					}
+				}
+				//update res
+				if( s.empty() || (heights[s.top()]<=heights[c]) ){
+					if(heights[c]){
+						if(s.empty()) l = c; 
+						s.push(c);
+					}
 				}
 				else{
-					heights[c] = 0;
+					while( (!s.empty()) && (heights[s.top()]>heights[c]) ){
+						int idx = s.top();
+						s.pop();
+						int temp = s.empty() ? heights[idx]*(c-l) : \
+								   heights[idx]*(c-s.top()-1);
+						res = max(res, temp);
+					}
+					if(heights[c]) s.push(c);
 				}
 			}
 		}
