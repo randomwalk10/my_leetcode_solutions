673c60de6f6f4b8247aff4c3800a2f987f9ab12b
Xuemeng Zhang <nxzhangxm@gmail.com>
2018-12-23 14:27:31 -0700

    add prob # 875; O(NlogW) in time and O(1) in space, where N is length of piles and W is range of max height for each pile

---
+++ 875_Koko_Eating_Bananas.cpp
@@ -0,0 +1,60 @@
+/*
+Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have gone and will come back in H hours.
+
+Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
+
+Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
+
+Return the minimum integer K such that she can eat all the bananas within H hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], H = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], H = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], H = 6
+Output: 23
+ 
+
+Note:
+
+1 <= piles.length <= 10^4
+piles.length <= H <= 10^9
+1 <= piles[i] <= 10^9
+*/
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+	bool isKValid(vector<int>& piles, int H, int K){
+		for(auto p: piles){
+			H -= (p-1)/K + 1;
+		}
+		return H>=0;
+	}
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+		int l=0;//lower bound, open
+		int h=1;//upper bound, close
+		//init h
+		for(int i=0; i<(int)piles.size(); ++i){
+			if(piles[i]>h) h = piles[i];
+		}
+		//binary search
+		while(l+1<h){
+			int m = (l+h)/2;
+			if(isKValid(piles, H, m)) h = m;
+			else l = m;
+		}
+		//return
+		return h;
+    }
+};
