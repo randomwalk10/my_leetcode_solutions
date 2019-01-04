df95a62c2ce2ca3399f2e6de67078bcc10e9107a
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-01-03 23:20:28 -0700

    add prob #112; O(N) in time and O(Height) in height; use recursion

---
+++ 112_Path_Sum.cpp
@@ -0,0 +1,47 @@
+/*
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#define NULL 0
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+		if(NULL==root) return false;
+
+		if( (NULL==root->left)&&(NULL==root->right)&&(root->val==sum) ) return true;
+		if(hasPathSum(root->left, sum-root->val)) return true;
+		if(hasPathSum(root->right, sum-root->val)) return true;
+		return false;
+    }
+};
