f712ccadff969c40b5c4f0f306983e307b2d1a52
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-02-04 23:43:26 -0700

    modify prob #212; build trie for words and perform dfs all together for trie; use more memory to reduce time

--- 212_Word_Search_II.cpp
+++ 212_Word_Search_II.cpp
@@ -20,67 +20,68 @@ You may assume that all inputs are consist of lowercase letters a-z.
 */
 #include <vector>
 #include <string>
-#include <unordered_set>
 using namespace std;
+
+struct Node{
+	Node(){};
+	Node* child_[26] = {NULL};
+	bool stop_ = false;
+};
 
 class Solution {
 private:
 	vector<vector<int>> diff_ = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-	bool findWordInBoardAtLoc(vector<vector<char>>& board, string& word, \
-			int idx, int r, int c, vector<vector<bool>>& is_visited){
-		if(idx==(int)word.size()) return true;
-		else if( (0>r)||(r>=(int)board.size())|| \
-				(0>c)||(c>=(int)board[0].size())|| \
-				is_visited[r][c]||(word[idx]!=board[r][c]) )
-				return false;
-		bool res = false;
-		//set is_visited[r][c]
-		is_visited[r][c] = true;
-		//dfs
+	void dfs(vector<vector<char>>& board, Node* p, \
+			int r, int c, vector<string>& res, string& path){
+		//check if valid or not
+		if( (r<0)||(c<0)||(r>=(int)board.size())||(c>=(int)board[0].size()) ) return;
+		if( board[r][c] == '#' ) return;//(r,c) is visited
+		if( NULL==p->child_[board[r][c]-'a'] ) return;//not found in trie
+		//update context
+		path.push_back(board[r][c]);
+		char temp_char = board[r][c];
+		board[r][c] = '#';//visited
+		//add to res if possible
+		p = p->child_[temp_char-'a'];
+		if(p->stop_){
+			res.push_back(path);
+			p->stop_ = false;//in case of duplicate
+		}
+		//search into neighbors
 		for(int i=0; i<(int)diff_.size(); ++i){
-			int new_r = r + diff_[i][0];
-			int new_c = c + diff_[i][1];
-			res = findWordInBoardAtLoc(board, word, idx+1, new_r, new_c, is_visited);
-			if(res) break;
+			dfs(board, p, r+diff_[i][0], c+diff_[i][1], res, path);
 		}
-		//restore is_visited[r][c]
-		is_visited[r][c] = false;
+		//restore context
+		board[r][c] = temp_char;
+		path.pop_back();
 		//return
-		return res;
+		return;
 	}
-	bool findWordInBoard(vector<vector<char>>& board, string& word, \
-			vector<vector<bool>>& is_visited){
-		if(word.empty()) return false;
-		//find matching char to start with
-		//then perform dfs to match the word
-		//return true if matching is found
-		for(int r=0; r<(int)board.size(); ++r){
-			for(int c=0; c<(int)board[0].size(); ++c){
-				if( findWordInBoardAtLoc(board, word, 0, r, c, is_visited) ){
-					return true;
-				}
+	void buildTrie(vector<string>& words, Node* head){
+		for(int i=0; i<(int)words.size(); ++i){
+			Node* p = head;
+			for(int j=0; j<(int)words[i].size(); ++j){
+				if(p->child_[words[i][j]-'a']==NULL)
+					p->child_[words[i][j]-'a'] = new Node();
+				p = p->child_[words[i][j]-'a'];
 			}
+			p->stop_ = true;
 		}
-
-		//return
-		return false;
 	}
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-		unordered_set<string> res_set;
 		vector<string> res;
-		vector<vector<bool>> is_visited(board.size(), vector<bool>(board[0].size(), false));
+		Node* head = new Node();
+		string path;
 
-		for(int i=0; i<(int)words.size(); ++i){
-			//dfs for each word
-			//append to res if true
-			if(findWordInBoard(board, words[i], is_visited))
-				res_set.insert(words[i]);
-		}
+		//build trie
+		buildTrie(words, head);
 
-		for(unordered_set<string>::iterator iter = res_set.begin(); \
-				iter != res_set.end(); ++iter){
-			res.push_back(*iter);
+		//dfs at each (r,c)
+		for(int r=0; r<(int)board.size(); ++r){
+			for(int c=0; c<(int)board[0].size(); ++c){
+				dfs(board, head, r, c, res, path);
+			}
 		}
 
 		return res;
