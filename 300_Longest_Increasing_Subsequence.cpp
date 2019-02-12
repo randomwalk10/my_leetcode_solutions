892d3421af78e5ee1b1c755e585f4865c81440ec
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-02-12 12:17:52 -0700

    modify prob #300; O(nlogn) in time and O(n) in space; use DP and Binary Search; The key point is to store the smallest tail numbers of subsequences of all lengths

--- 300_Longest_Increasing_Subsequence.cpp
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -21,18 +21,36 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
 		int n = nums.size();
-		if(n==0) return 0;
-		vector<int> dp(n, 1);
+		if(n<=1) return n;
+
+		vector<int> dp(n, 0);//store smallest tail number in subsequences of length at index length - 1
 		int res = 1;//length of longest subsequences
 
 		for(int i=0; i<n; ++i){
-			for(int j=0; j<i; ++j){
-				if( (nums[i]>nums[j])&& \
-						(dp[j]+1>dp[i]) ){
-					dp[i] = dp[j]+1;
+			if(0==i){
+				dp[0] = nums[0];
+			}
+			else{
+				//perform binary search
+				int l = 0;//where nums[i]>dp[l]
+				int h = res-1;//where nums[i]<=dp[h]
+				if(nums[i]<=dp[l])
+					dp[l] = nums[i];
+				else if(nums[i]>dp[h]){
+					dp[h+1] = nums[i];
+					++res;
+				}
+				else{
+					//find max l
+					while(l+1<h){
+						int m = (l+h)/2;
+						if(nums[i]>dp[m]) l = m;
+						else h = m;
+					}
+					//update dp[l+1]
+					dp[l+1] = (nums[i]<dp[l+1]) ? nums[i] : dp[l+1];
 				}
 			}
-			res = (dp[i]>res) ? dp[i] : res;
 		}
 
 		return res;
