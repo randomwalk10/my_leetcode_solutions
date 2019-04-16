d1801b52760758d0daf9f7a3e150ce67054d272a
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-04-16 12:53:49 +0800

    add prob #474; O(k*m*n) in time and O(m*n) in space, where k is the length of strs array, m is the number of zeros, and n is the number of ones; use dynamic programming

---
+++ 474_Ones_and_Zeroes.cpp
@@ -0,0 +1,63 @@
+/*
+In the computer world, use restricted resource you have to generate maximum benefit is what we always want to pursue.
+
+For now, suppose you are a dominator of m 0s and n 1s respectively. On the other hand, there is an array with strings consisting of only 0s and 1s.
+
+Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can be used at most once.
+
+Note:
+
+The given numbers of 0s and 1s will both not exceed 100
+The size of given string array won't exceed 600.
+Example 1:
+
+Input: Array = {"10", "0001", "111001", "1", "0"}, m = 5, n = 3
+Output: 4
+
+Explanation: This are totally 4 strings can be formed by the using of 5 0s and 3 1s, which are “10,”0001”,”1”,”0”
+Example 2:
+
+Input: Array = {"10", "0", "1"}, m = 1, n = 1
+Output: 2
+
+Explanation: You could form "10", but then you'd have nothing left. Better form "0" and "1".
+*/
+
+#include <vector>
+#include <utility>
+#include <string>
+using namespace std;
+
+class Solution {
+private:
+	void countOnesAndZeros(string& input, int& zeros, int& ones){
+		ones = 0;
+		zeros = 0;
+		for(int i=0; i<(int)input.size(); ++i){
+			if(input[i]=='1') ++ones;
+			else ++zeros;
+		}
+	}
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+		if(strs.empty()) return 0;
+		if( (0==m)&&(0==n) ) return 0;
+		// dynamic programming
+		vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+		for(int i=0; i<(int)strs.size(); ++i){
+			// get the current numbers of 1's and 0's at strs[i]
+			int ones, zeros;
+			countOnesAndZeros(strs[i], zeros, ones);
+			// update dp
+			for(int r=m; r>=0; --r){
+				for(int c=n; c>=0; --c){
+					if( (r>=zeros) && (c>=ones) ){
+						dp[r][c] = max(dp[r][c], dp[r-zeros][c-ones]+1);
+					}
+				}
+			}
+		}
+		// return
+		return dp[m][n];
+    }
+};
