c87c8ce65483984e2e31083b166c512122d86541
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-04-23 22:13:03 +0800

    modify prob #622; O(n^2) in time and O(1) in space; beat 100% sol in both time and space

--- 611_Valid_Triangle_Number.cpp
+++ 611_Valid_Triangle_Number.cpp
@@ -19,32 +19,25 @@ The integers in the given array are in the range of [0, 1000].
 using namespace std;
 
 class Solution {
-private:
-	bool isTriangle(int a, int b, int c){// presume c >= max(a, b)
-		return a+b>c;
-	}
 public:
     int triangleNumber(vector<int>& nums) {
 		if(nums.size()<3) return 0;
 		int res = 0;
-		vector<int> dp(1002, 0);//dp[1001] stores the count of the sum of any pair of sides that is greater than 1000
 		// sort nums
 		std::sort(nums.begin(), nums.end());
 
 		// find valid triangles
-		for(int i=2; i<(int)nums.size(); ++i){
-			int j = i-1;
-			// find triangles of (a, b, i) where a<j and b<j
-			for(int k=nums[i]+1; k<1002; ++k){
-				res += dp[k];
-			}
-			// find triangles of (k, j, i) where k<j
-			for(int k=j-1; k>=0; --k){
-				if(nums[k]+nums[j]>nums[i]){
-					++dp[min(1001, nums[k]+nums[j])];
-					++res;
+		for(int i=nums.size()-1; i>=2; --i){
+			int j = i - 1; //  the midle side
+			int k = 0; // the shortest side
+			while(j>k){
+				if(nums[j]+nums[k]>nums[i]){
+					res += j - k;
+					--j;
 				}
-				else break;
+				else{
+					++k;
+				}
 			}
 		}
 
