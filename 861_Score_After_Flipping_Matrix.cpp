b94272295153c52b8339a611c1dd3b83d8635bac
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-05-28 17:59:37 +0800

    add prob #861; O(M*N) in time and O(M) in space, where M is rows and N is columns; use Greedy Algorithm where the first columns have to be ones' and the following columns(left to right) have to be flipped if ones is less than zeros

---
+++ 861_Score_After_Flipping_Matrix.cpp
@@ -0,0 +1,54 @@
+/*
+We have a two dimensional matrix A where each value is 0 or 1.
+
+A move consists of choosing any row or column, and toggling each value in that row or column: changing all 0s to 1s, and all 1s to 0s.
+
+After making any number of moves, every row of this matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
+
+Return the highest possible score.
+
+ 
+
+Example 1:
+
+Input: [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+Output: 39
+Explanation:
+Toggled to [[1,1,1,1],[1,0,0,1],[1,1,1,1]].
+0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& A) {
+		int res = 0;
+		int rows = A.size(), cols = A[0].size();
+		vector<int> row_flips(rows, 0);
+
+		// iterate through each column
+		for(int c=0; c<cols; ++c){
+			int tmp = 0;
+			// iterate through each row
+			for(int r=0; r<rows; ++r){
+				if(0==c){
+					if(A[r][c]==0) row_flips[r] = 1;
+					++tmp;
+				}
+				else{
+					tmp += A[r][c]^row_flips[r];
+				}
+			}
+			// flip tmp if tmp <= rows / 2
+			// this is column flip
+			if(tmp<=(rows>>1)) tmp = rows - tmp;
+			// update res
+			res = (res<<1) + tmp;
+		}
+
+		// return
+		return res;
+    }
+};
