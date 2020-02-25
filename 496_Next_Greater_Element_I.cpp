a8863180bcddfd3e9ead1c52c069efddc6b304aa
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-02-25 23:32:00 +0800

    add prob #496; O(N) in time and O(N) in space;

---
+++ 496_Next_Greater_Element_I.cpp
@@ -0,0 +1,58 @@
+/*
+You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+Output: [-1,3,-1]
+Explanation:
+    For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+    For number 1 in the first array, the next greater number for it in the second array is 3.
+    For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+Example 2:
+Input: nums1 = [2,4], nums2 = [1,2,3,4].
+Output: [3,-1]
+Explanation:
+    For number 2 in the first array, the next greater number for it in the second array is 3.
+    For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+Note:
+All elements in nums1 and nums2 are unique.
+The length of both nums1 and nums2 would not exceed 1000.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-greater-element-i
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/* O(N) in time and O(N) in space */
+#include <vector>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> val2idx;
+        vector<int> res(nums1.size(), -1);
+        stack<int> s;
+
+        for(int i=0; i<(int)nums1.size(); ++i){
+            val2idx[nums1[i]] = i;
+        }
+
+        for(int i=0; i<(int)nums2.size(); ++i){
+            while( (!s.empty())&&(s.top()<nums2[i]) ){
+                res[val2idx[s.top()]] = nums2[i];
+                s.pop();
+            }
+
+            if(val2idx.end()!=val2idx.find(nums2[i])){
+                s.push(nums2[i]);
+            }
+        }
+
+        return res;
+    }
+};
