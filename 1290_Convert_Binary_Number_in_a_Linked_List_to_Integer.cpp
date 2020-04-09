2d4be50721caac03d043d7ec2fb407bf8ba34483
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-04-09 23:12:18 +0800

    add prob #1290; O(N) in time and O(1) in space

---
+++ 1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -0,0 +1,74 @@
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+Example 1:
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+
+Input: head = [0]
+Output: 0
+
+Example 3:
+
+Input: head = [1]
+Output: 1
+
+Example 4:
+
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+
+Example 5:
+
+Input: head = [0,0]
+Output: 0
+ 
+
+Constraints:
+
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <stdlib.h>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+
+        while(head){
+            res <<= 1;
+            if(head->val){
+                ++res;
+            }
+            head = head->next;
+        }
+
+        return res;
+    }
+};
