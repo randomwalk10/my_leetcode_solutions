1384f057131feb4a9b939397508cf7d01fbc3ffa
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-05-31 05:14:17 +0000

    add pr938

---
+++ 938_Range_Sum_of_BST.cpp
@@ -0,0 +1,48 @@
+/*
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+ 
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+ 
+
+Note:
+
+The number of nodes in the tree is at most 10000.
+The final answer is guaranteed to be less than 2^31.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-of-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/* O(N) in time and O(N) in space */
+#define NULL 0
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(root==NULL) return 0;
+        if(root->val<L) return rangeSumBST(root->right, L, R);
+        if(root->val>R) return rangeSumBST(root->left, L, R);
+        int res = root->val;
+        res += rangeSumBST(root->left, L, root->val);
+        res += rangeSumBST(root->right, root->val, R);
+        return res;
+    }
+};
