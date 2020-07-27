10ee6da08b1705f30afbc6bd850acb5a0ef7dc48
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-07-27 10:56:26 +0000

    add prob #671; iterative solution

---
+++ 671_Second_Minimum_Node_In_a_Binary_Tree.cpp
@@ -0,0 +1,65 @@
+/*
+Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+
+Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+
+If no such second minimum value exists, output -1 instead.
+
+Example 1:
+
+Input: 
+    2
+   / \
+  2   5
+     / \
+    5   7
+
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+ 
+
+Example 2:
+
+Input: 
+    2
+   / \
+  2   2
+
+Output: -1
+Explanation: The smallest value is 2, but there isn't any second smallest value.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+		if(NULL==root) return -1;
+		stack<TreeNode*> s;
+		int min_val = root->val;
+		int res = -1;
+
+		s.push(root);
+		while(!s.empty()){
+			TreeNode* tmp = s.top(); s.pop();
+
+			if(tmp->val>min_val) res = (res>=0)?min(res, tmp->val):tmp->val;
+			if(tmp->left) s.push(tmp->left);
+			if(tmp->right) s.push(tmp->right);
+		}
+
+		return res;
+    }
+};
