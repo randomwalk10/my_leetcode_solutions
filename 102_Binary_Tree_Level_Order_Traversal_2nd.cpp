39cc647fdc6cf71566a4e8f23d4630723ad607a5
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-08-31 14:48:03 +0000

    add prob #102(2nd); iterative  solution

---
+++ 102_Binary_Tree_Level_Order_Traversal_2nd.cpp
@@ -0,0 +1,48 @@
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> res;
+		stack<TreeNode*> s;
+		stack<int> s_d;
+
+		if(root){
+			s.push(root);
+			s_d.push(0);
+		}
+		while(!s.empty()){
+			TreeNode* tmp = s.top(); s.pop();
+			int d = s_d.top(); s_d.pop();
+
+			if(d>=res.size()){
+				res.push_back({tmp->val});
+			}
+			else{
+				res[d].push_back(tmp->val);
+			}
+
+			//right
+			if(tmp->right){
+				s.push(tmp->right);
+				s_d.push(d+1);
+			}
+			//left
+			if(tmp->left){
+				s.push(tmp->left);
+				s_d.push(d+1);
+			}
+		}
+
+		return res;
+    }
+};
