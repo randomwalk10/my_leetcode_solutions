a20f21469fd4abd8aaa1e191dd9bb4c061014e66
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-10-26 07:25:43 +0000

    add prob #1221; O(n) in time and O(1) in space

---
+++ 1221_Split_a_String_in_Balanced_Strings.cpp
@@ -0,0 +1,61 @@
+/*
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] = 'L' or 'R'
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/split-a-string-in-balanced-strings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+		int res = 0;
+		int pos = 0;
+		for (size_t i = 0; i < s.size(); ++i) {
+			if(s[i]=='L'){
+				--pos;
+			}
+			else{
+				++pos;
+			}
+			if(pos==0) ++res;
+		}
+		return res;
+    }
+};
