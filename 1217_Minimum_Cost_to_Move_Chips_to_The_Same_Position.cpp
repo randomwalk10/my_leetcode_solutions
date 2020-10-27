d4cb0058c710e7da72ad663743c9ed1ef4129ef9
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-10-27 07:22:44 +0000

    add prob #1217; O(n) in time and O(1) in space

---
+++ 1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position.cpp
@@ -0,0 +1,57 @@
+/*
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+ 
+
+Example 1:
+
+
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+Example 2:
+
+
+Input: position = [2,2,2,3,3]
+Output: 2
+Explanation: We can move the two chips at poistion 3 to position 2. Each move has cost = 1. The total cost = 2.
+Example 3:
+
+Input: position = [1,1000000000]
+Output: 1
+ 
+
+Constraints:
+
+1 <= position.length <= 100
+1 <= position[i] <= 10^9
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-cost-to-move-chips-to-the-same-position
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+		int odds = 0, evens = 0;
+
+		for (size_t i = 0; i < position.size(); ++i) {
+			if(position[i]%2) ++odds;
+			else ++evens;
+		}
+
+		return min(odds, evens);
+    }
+};
