62796ae00bcc8148729938dc876644dc39ae0844
Xuemeng Zhang <nxzhangxm@gmail.com>
2020-10-28 12:59:58 +0000

    add prob #1518; O(logN) in time and O(1) in space

---
+++ 1518_Water_Bottles.cpp
@@ -0,0 +1,65 @@
+/*
+Given numBottles full water bottles, you can exchange numExchange empty water bottles for one full water bottle.
+
+The operation of drinking a full water bottle turns it into an empty bottle.
+
+Return the maximum number of water bottles you can drink.
+
+ 
+
+Example 1:
+
+
+
+Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+Number of water bottles you can drink: 9 + 3 + 1 = 13.
+Example 2:
+
+
+
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+Number of water bottles you can drink: 15 + 3 + 1 = 19.
+Example 3:
+
+Input: numBottles = 5, numExchange = 5
+Output: 6
+Example 4:
+
+Input: numBottles = 2, numExchange = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= numBottles <= 100
+2 <= numExchange <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/water-bottles
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+		int res = 0;
+		int empty_bottles = 0;
+
+		res += numBottles;
+		empty_bottles += numBottles;
+		while(empty_bottles>=numExchange){
+			// exchange
+			numBottles = empty_bottles / numExchange;
+			empty_bottles = empty_bottles % numExchange;
+			// drink
+			res += numBottles;
+			empty_bottles += numBottles;
+		}
+
+		return res;
+    }
+};
