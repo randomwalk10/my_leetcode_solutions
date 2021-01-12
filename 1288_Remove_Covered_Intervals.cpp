76e12ac92231faeeffd8a07a195670f36eeaa65e
Xuemeng Zhang <nxzhangxm@gmail.com>
2021-01-12 17:02:25 +0800

    add prob #1288; O(nlogn) in time and O(1) in space

---
+++ 1288_Remove_Covered_Intervals.cpp
@@ -0,0 +1,67 @@
+/*
+Given a list of intervals, remove all intervals that are covered by another interval in the list.
+
+Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
+
+After doing so, return the number of remaining intervals.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+Example 2:
+
+Input: intervals = [[1,4],[2,3]]
+Output: 1
+Example 3:
+
+Input: intervals = [[0,10],[5,12]]
+Output: 2
+Example 4:
+
+Input: intervals = [[3,10],[4,10],[5,11]]
+Output: 2
+Example 5:
+
+Input: intervals = [[1,2],[1,4],[3,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 1000
+intervals[i].length == 2
+0 <= intervals[i][0] < intervals[i][1] <= 10^5
+All the intervals are unique.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-covered-intervals
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& lhs, const vector<int>& rhs){
+            return (lhs[0]==rhs[0])?lhs[1]>rhs[1]:lhs[0]<rhs[0];
+        });
+
+        int prev_end = 0;
+        int res = 0;
+        for(auto interval : intervals){
+            if(interval[1]>prev_end){
+                prev_end = interval[1];
+                ++res;
+            }
+        }
+
+        return res;
+    }
+};
